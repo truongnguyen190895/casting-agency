2a940cba7315b01e1fc07020210295234bdd0c69
truong <ntruong@switch.tv>
2023-11-04 10:48:42 +0700

    implement models and GET,DELETE routes

---
+++ app.py
@@ -0,0 +1,160 @@
+import os
+from flask import Flask, jsonify, request
+from models import setup_db, Actor, Movie
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization"
+        )
+        return response
+
+    @app.route("/")
+    def get_greeting():
+        excited = os.environ["EXCITED"]
+        greeting = "Hello"
+        if excited == "true":
+            greeting = greeting + "!!!!! You are doing great in this Udacity project."
+        return greeting
+
+    @app.route("/actors", methods=["GET"])
+    def get_actors():
+        actors = Actor.query.all()
+        formatted_actors = [actor.format() for actor in actors]
+        return jsonify(
+            {
+                "success": True,
+                "actors": formatted_actors,
+            }
+        )
+
+    @app.route("/movies", methods=["GET"])
+    def get_movies():
+        movies = Movie.query.all()
+        formatted_movies = [movie.format() for movie in movies]
+        return jsonify(
+            {
+                "success": True,
+                "movies": formatted_movies,
+            }
+        )
+
+    @app.route("/actors/<int:actor_id>", methods=["DELETE"])
+    def delete_actor(actor_id):
+        actor = Actor.query.get(actor_id)
+        if actor is None:
+            return (
+                jsonify(
+                    {
+                        "success": False,
+                        "error": "Actor not found",
+                    }
+                ),
+                404,
+            )
+
+        actor.delete()
+        return jsonify(
+            {
+                "success": True,
+                "deleted_actor_id": actor.id,
+            }
+        )
+
+    @app.route("/movies/<int:movie_id>", methods=["DELETE"])
+    def delete_movie(movie_id):
+        movie = Movie.query.get(movie_id)
+        if movie is None:
+            return (
+                jsonify(
+                    {
+                        "success": False,
+                        "error": "Movie not found",
+                    }
+                ),
+                404,
+            )
+
+        movie.delete()
+        return jsonify(
+            {
+                "success": True,
+                "deleted_movie_id": movie.id,
+            }
+        )
+
+    @app.route("/actors", methods=["POST"])
+    def create_actor():
+        data = request.get_json()
+
+        name = data.get("name")
+        age = data.get("age")
+        gender = data.get("gender")
+
+        if not name or not age or not gender:
+            return (
+                jsonify(
+                    {
+                        "success": False,
+                        "error": "Please provide valid name, age, and gender for the actor.",
+                    }
+                ),
+                400,
+            )
+
+        actor = Actor(name=name, age=age, gender=gender)
+        actor.insert()
+
+        return jsonify(
+            {
+                "success": True,
+                "created_actor_id": actor.id,
+            }
+        )
+
+    @app.route("/movies", methods=["POST"])
+    def create_movie():
+        data = request.get_json()
+
+        title = data.get("title")
+        release_date = data.get("release_date")
+
+        if not title or not release_date:
+            return (
+                jsonify(
+                    {
+                        "success": False,
+                        "error": "Please provide a valid title and release date for the movie.",
+                    }
+                ),
+                400,
+            )
+
+        movie = Movie(title=title, release_date=release_date)
+        movie.insert()
+
+        return jsonify(
+            {
+                "success": True,
+                "created_movie_id": movie.id,
+            }
+        )
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,70 @@
+import os
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+# database_path = os.environ["DATABASE_URL"]
+database_path = "postgresql://postgres:123456@localhost:5432/casting_agency"
+if database_path.startswith("postgres://"):
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
+
+db = SQLAlchemy()
+
+"""
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+"""
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+
+    def format(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date.strftime(
+                "%Y-%m-%d"
+            ),  # Format the date as a string
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
